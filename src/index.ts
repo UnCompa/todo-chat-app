212fb19c5c5ff5953d2d12ea28078acf8e1e6639
UnCompa <brandonddxd@gmail.com>
2025-09-15 17:50:25 -0500

    feat: adddig changes

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import pino from 'pino-http'
+import express from 'express'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express()
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(pino({
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true
+        }
+    }
+}))
+
+app.get('/', function (req, res) {
+    req.log.info('something')
+    res.send('hello world')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
